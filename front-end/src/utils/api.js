70ff0518a461280fa3add131a14270f02abdae38
Keri Medeiros <kmedeiros@google.com>
2023-03-11 16:02:17 -0800

    Updating server URL

--- front-end/src/utils/api.js
+++ front-end/src/utils/api.js
@@ -6,9 +6,8 @@ import formatReservationDate from "./format-reservation-date";
 import formatReservationTime from "./format-reservation-date";
 
 const API_BASE_URL =
-  process.env.REACT_APP_API_BASE_URL || "http://localhost:5001";
-// process.env.REACT_APP_API_BASE_URL || "http://localhost:5001";
-// "https://periodic-tables-backend-cjr.herokuapp.com"
+  process.env.REACT_APP_API_BASE_URL || "https://restaurant-reservation-app-backend-xstd.onrender.com"
+
 
 /**
  * Defines the default headers for these functions to work with `json-server`
